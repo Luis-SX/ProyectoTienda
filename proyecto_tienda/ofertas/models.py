301dd68d95e76eb6efb124a2da1af809deb2a97c
Luis-SX <lguzmang@vivaldi.net>
2024-11-06 19:44:40 -0300

    modifique algunas cosas y agregué imagen a ofertas

--- proyecto_tienda/ofertas/models.py
+++ proyecto_tienda/ofertas/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from productos.models import Producto
 
 # Create your models here.
+class Oferta(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    porcentaje_descuento =models.DecimalField(max_digits=6, decimal_places=2)
+    fecha_inicio = models.DateTimeField()
+    fecha_fin = models.DateTimeField()
 
+    def __str__(self):
+        return f" oferta en: {self.producto} --- Descuento: {self.porcentaje_descuento} %"
+
+    @property
+    def precio_descuento(self):
+        return self.producto.precio - (self.producto.precio * self.porcentaje_descuento / 100)
--- proyecto_tienda/ofertas/views.py
+++ proyecto_tienda/ofertas/views.py
@@ -1,5 +1,16 @@
+from datetime import datetime
 from django.shortcuts import render
+from .models import Oferta
 
 # Create your views here.
+current_date = datetime.now()
+ofertas = Oferta.objects.filter(fecha_inicio__lte=current_date, 
+fecha_fin__gte=current_date)
+
 def index(request):
-    return render(request, 'ofertas/index.html')
+    context={
+        'current_date': current_date,
+        #is_special_offer: False #false para no y true para si
+        'ofertas': ofertas
+    }
+    return render(request, 'ofertas/index.html', context)
