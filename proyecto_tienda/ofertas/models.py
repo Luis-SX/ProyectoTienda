f5d4b65516efe4d1018e8679d052ce85718c9b0a
Luis-SX <lguzmang@vivaldi.net>
2024-11-06 20:20:03 -0300

    Agregado validacion de errores

--- proyecto_tienda/ofertas/models.py
+++ proyecto_tienda/ofertas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 from productos.models import Producto
 
 # Create your models here.
@@ -7,6 +8,16 @@ class Oferta(models.Model):
     porcentaje_descuento =models.DecimalField(max_digits=6, decimal_places=2)
     fecha_inicio = models.DateTimeField()
     fecha_fin = models.DateTimeField()
+
+    def clean(self):   #metodo especial para validaciones personalizadas
+        if self.fecha_inicio >= self.fecha_fin:
+            raise ValidationError('La fecha de inicio debe ser menor que la fecha de fin')
+        if self.porcentaje_descuento < 0 or self.porcentaje_descuento > 100:
+            raise ValidationError('El porcentaje de descuento debe estar entre 0 y 100')
+
+    def save(self, *args, **kwargs):    #invoca a Clean antes de guardar los datos
+        self.clean()
+        super(Oferta, self).save(*args, **kwargs)
 
     def __str__(self):
         return f" oferta en: {self.producto} --- Descuento: {self.porcentaje_descuento} %"
--- proyecto_tienda/ofertas/views.py
+++ proyecto_tienda/ofertas/views.py
@@ -3,11 +3,24 @@ from django.shortcuts import render
 from .models import Oferta
 
 # Create your views here.
-current_date = datetime.now()
-ofertas = Oferta.objects.filter(fecha_inicio__lte=current_date, 
-fecha_fin__gte=current_date)
 
 def index(request):
+    current_date = datetime.now()
+    ofertas=[]
+
+    try:
+        ofertas = Oferta.objects.filter(fecha_inicio__lte=current_date, 
+        fecha_fin__gte=current_date)
+        if not ofertas:
+            raise ValueError("no hay ofertas disponibles en este momento")
+    except ValueError as e:
+        #manejo de error especifico  
+        return render(request, 'ofertas/index.html', {'error': str(e), 
+        'current_date': current_date})
+    except Exception as e:
+        #Manejo de cualquier otro error       
+        return render(request, 'ofertas/index.xtml', 
+        {'error': 'Se produjo  un errror inesperado!', 'current_date': current_date})
     context={
         'current_date': current_date,
         #is_special_offer: False #false para no y true para si
