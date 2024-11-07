7bed5aea850a0e64d09e3105e2074a6a8acb16a0
Luis-SX <lguzmang@vivaldi.net>
2024-11-06 21:00:41 -0300

    corrección de error

--- proyecto_tienda/ofertas/views.py
+++ proyecto_tienda/ofertas/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.shortcuts import render
 from .models import Oferta
+from django.http import Http404 
 
 # Create your views here.
 
@@ -11,6 +12,7 @@ def index(request):
     try:
         ofertas = Oferta.objects.filter(fecha_inicio__lte=current_date, 
         fecha_fin__gte=current_date)
+        raise Exception("no hay ofertas disponibles en este momento")
         if not ofertas:
             raise ValueError("no hay ofertas disponibles en este momento")
     except ValueError as e:
@@ -19,7 +21,7 @@ def index(request):
         'current_date': current_date})
     except Exception as e:
         #Manejo de cualquier otro error       
-        return render(request, 'ofertas/index.xtml', 
+        return render(request, 'ofertas/index.html', 
         {'error': 'Se produjo  un errror inesperado!', 'current_date': current_date})
     context={
         'current_date': current_date,
