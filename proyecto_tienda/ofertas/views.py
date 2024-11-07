f9e1c160ebfe95f8d860b44b1dc3226868562ba6
Luis-SX <lguzmang@vivaldi.net>
2024-11-06 22:24:35 -0300

    se agregan restricciones y request para el formulario

--- proyecto_tienda/ofertas/views.py
+++ proyecto_tienda/ofertas/views.py
@@ -1,9 +1,21 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Oferta
-from django.http import Http404 
+from .forms import OfertaForm
 
 # Create your views here.
+
+def crear_oferta(request):
+    if request.method == 'POST':
+        form=OfertaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ofertas:index')
+    else:
+        form=OfertaForm
+    return render(request, 'ofertas/crear_oferta.html', {'form': form}) 
+
+
 
 def index(request):
     current_date = datetime.now()
@@ -12,7 +24,7 @@ def index(request):
     try:
         ofertas = Oferta.objects.filter(fecha_inicio__lte=current_date, 
         fecha_fin__gte=current_date)
-        raise Exception("no hay ofertas disponibles en este momento")
+        #raise Exception("no hay ofertas disponibles en este momento")
         if not ofertas:
             raise ValueError("no hay ofertas disponibles en este momento")
     except ValueError as e:
@@ -22,7 +34,7 @@ def index(request):
     except Exception as e:
         #Manejo de cualquier otro error       
         return render(request, 'ofertas/index.html', 
-        {'error': 'Se produjo  un errror inesperado!', 'current_date': current_date})
+        {'error': 'Se produjo  un error inesperado!', 'current_date': current_date})
     context={
         'current_date': current_date,
         #is_special_offer: False #false para no y true para si
