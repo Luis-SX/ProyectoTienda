703095ff868d3bf36d7092a0febe876cb32d334d
Luis-SX <lguzmang@vivaldi.net>
2024-11-07 21:02:49 -0300

    se agrega la funcion def save_model para los permisos de admin

--- proyecto_tienda/ofertas/admin.py
+++ proyecto_tienda/ofertas/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Oferta
+from django.contrib.auth.models import Group, Permission
 
 # Register your models here.
 @admin.register(Oferta)
@@ -7,3 +8,8 @@ from .models import Oferta
 class OfertaAdmin(admin.ModelAdmin):
     list_display = ('producto', 'porcentaje_descuento', 'fecha_inicio', 'fecha_fin')
     search_fields = ('producto__nombre', )
+
+    def save_model(self, request, obj, form, change):   #Permisos para el grupo administrador
+        group_admin = Group.objects.get(name='Administradores')
+        permisos = Permission.objects.filter(codename__in=['crear_ofertas', 'editar_ofertas', 'eliminar_ofertas'])
+        group_admin.permissions.add(*permisos)
